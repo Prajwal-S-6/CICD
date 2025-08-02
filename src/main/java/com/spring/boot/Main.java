827df8e3e0fd2f603767a067765bf4dc4d0705b1
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-08-02 18:29:30 +0530

    jenkins

--- src/main/java/com/spring/boot/Main.java
+++ src/main/java/com/spring/boot/Main.java
@@ -1,14 +1,11 @@
-package com.spring.boot.test;
+package com.spring.boot;
 
-import com.spring.boot.dao.CityDao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.spring.boot.dao", "com.spring.boot.ds", "com.spring.boot.service", "com.spring.boot.controller"})
-@EnableJpaRepositories(basePackages = "com.spring.boot.dao")
-@EntityScan("com.spring.boot.ds")
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
